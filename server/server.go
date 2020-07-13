436cf1b7ddf6866c78c21153f83abaa6d651aed7
stankovic98 <antoniostankovic123@gmail.com>
2020-07-13 10:32:21 +0200

    diocese to parish changed in logs

--- server/server.go
+++ server/server.go
@@ -41,7 +41,7 @@ func (s *Server) mainDispatcher(w http.ResponseWriter, r *http.Request) {
 	parishID := urlPaths[2]
 	parish, err := s.Repo.GetParish(dioceseID, parishID)
 	if err == sql.ErrNoRows {
-		w.Write([]byte("diocese with id " + parishID + " doesn't exist\n"))
+		w.Write([]byte("parish with id " + parishID + " doesn't exist\n"))
 		return
 	}
 	json.NewEncoder(w).Encode(parish)
